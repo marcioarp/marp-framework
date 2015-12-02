c34cdba80f285043454f90ae0d992c350745eb1c
unknown <marcio.pinto@desk153.cemaden.gov.br>
2015-12-02 09:19:33 -0200

    02/12/2015

--- query.php
+++ query.php
@@ -326,6 +326,7 @@ class Query {
 		return $this->associaArrFromUser($newValues,$setNull) ;
 	}
 	
+	/*
 	public function associaArrFromUser($arr,$setNull=true) {
 		$retorno = true;
 		for ($i=0;$i<sizeof($this->arrCampos);$i++) {
@@ -333,42 +334,44 @@ class Query {
 			$campo = $this->$nomeCampo;
 			if (isset($arr[$nomeCampo])) {
 				$campo->setValue($arr[$nome<?php
-
-
-class BancoDados {
-	//variáveis do bd
-	public $id_banco_dados; 
-	public $server;
-	public $user ;
-	public $password; 
-	public $port; 
-	public $database_name; 
-	public $description; 
-	public $sgdb=SGDB_POSTGRES; //Define qual o Gerenciador do Banco de Dados (MySQL / PostgreSQL)
+	*/
+	public function associaArrFromUser($arr,$setNull=true) {
+		$retorno = true;
+		for ($i=0;$i<sizeof($this->arrCampos);$i++) {
+			$nomeCampo = $this->arrCampos[$i];
+			$campo = $this->$nomeCampo;
+			if (isset($arr[$nomeCampo])) {
+				$campo->setValue($arr[$nomeCampo]);
+			} else {
+				if($setNull) $campo->setNull();
+			}
+		}	
+		return $retorno;
+		
+	}
 	
-	//Métodos//
-	//Construtor	
-	function __construct($pid_banco_dados=0) {
-		//conexão ao bd_comum
-		if ($pid_banco_dados == 'C') {
-			$this->server=BD_COMUM_LOCAL_SERVER;
-			$this->user=BD_COMUM_LOCAL_USER;
-			$this->password=BD_COMUM_LOCAL_PASSWORD;
-			$this->port=BD_COMUM_LOCAL_PORT;
-			$this->database_name=BD_COMUM_LOCAL_DATABASE;
-			$this->description = 'Conexão ao BD Comum';
-			$this->sgdb=BD_COMUM_LOCAL_SGDB;
-			
-		} else if ($pid_banco_dados == 'CP') { //comum producao
-			$this->server=BD_COMUM_PRODUCAO_SERVER;
-			$this->user=BD_COMUM_PRODUCAO_USER;
-			$this->password=BD_COMUM_PRODUCAO_PASSWORD;
-			$this->port=BD_COMUM_PRODUCAOL_PORT;
-			$this->database_name=BD_COMUM_PRODUCAO_DATABASE;
-			$this->description = 'Conexão ao BD Comum';
-			$this->sgdb=BD_COMUM_PRODUCAO_SGDB;
-		} else if ($pid_banco_dados == 'P') { //Producao
-			$this->server=BD_APP_PRODUCAO_SERVER;
-			$this->user=BD_APP_PRODUCAO_USER;
-			$this->password=BD_APP_PRODUCAO_PASSWORD;
-			$this->port=
+	
+	public function eof() {return $this->_eof;}
+
+	public function bof() {return $this->_bof;}
+	
+	//executa sql e retorna um array com todos os dados
+	//não altera nenhum atributo do objeto instanciado
+	
+	public function sql2Array($sql) {
+		$rs = $this->cnx->runSQl($sql);
+		$v = $this->cnx->fetchAll($rs,MYSQL_ASSOC,true);
+		return $v;
+	}
+	
+	public function runSQL($sql) {
+		return $this->cnx->runSQL($sql);
+	}
+	 
+	 
+	
+	//Este método deve ser  chamado ao final do construct de todas as classes filhas
+	//public function outrasDefinicoes () { }
+	
+}
+?>	
